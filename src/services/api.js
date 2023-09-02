cac809f8b81bfbfdf30a3ebe5e0d4e8659c22431
rishijain21 <rishi987654321jain@gmail.com>
2023-09-02 15:52:42 +0530

    commitcs

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = 'c098ad9af4f9bd91ecd6a30eae40b9a1';
-const API_URL = 'http://api.openweathermap.org/data/2.5/weather'
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
 export const getWeather = async (city,country) =>{
 
